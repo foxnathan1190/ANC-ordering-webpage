e20152e6e414107da627c04f0ca6284901480122
foxnathan1190 <foxnathan1190@gmail.com>
2026-02-07 16:46:02 -0600

    Setting up links for home page.

--- ANC-ordering/src/components/Home.jsx
+++ ANC-ordering/src/components/Home.jsx
@@ -1,8 +1,23 @@
+import NavigationMenu from '../common/NavigationMenu';
+
 const Home = () => {
   return (
     <div className="home">
+      <NavigationMenu />
       <h1>Welcome to the Animal Nutrition Center Ordering System</h1>
       <p>Please choose your department</p>
+      <ul>
+        <li>Antelope</li>
+        <li>Bird House</li>
+        <li>Carnivore</li>
+        <li>Herpetarium</li>
+        <li>Insectarium</li>
+        <li>JOTA</li>
+        <li>Penguin and Puffin</li>
+        <li>Primate House</li>
+        <li>River's Edge</li>
+        <li>Sea Lion</li>
+      </ul>
     </div>
   )
 }
--- ANC-ordering/src/context/DataContext.jsx
+++ ANC-ordering/src/context/DataContext.jsx
@@ -4,5 +4,5 @@ export const DataContext = createContext();
 
 export const DataProvider = ({ children }) => {
 
-return <DataContext.Provider value={{ }}>{children}</DataContext.Provider>
+    return <DataContext.Provider value={{}}>{children}</DataContext.Provider>
 };
